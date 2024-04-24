237fb7f91a990badc20d3e0a26d27c2eaf30c5af
Omnia-Mohamed88 <https://github.com/Omnia-Mohamed88>
2024-04-24 05:23:50 +0200

    updated function index in the post controller

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,4 +1,7 @@
 class PostController < ApplicationController
     def index 
+        # post = Post.create({title: "t1" , content: "content1"})
+        post = Post.all
+        puts "+++++++++++++++==#{post.inspect}"
     end
    end
