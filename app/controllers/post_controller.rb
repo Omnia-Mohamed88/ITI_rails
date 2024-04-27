c37de53edb75089a5c6ca2e67f249433ef493267
Omnia-Mohamed88 <https://github.com/Omnia-Mohamed88>
2024-04-27 14:33:24 +0300

    modified the display functioalit and added the add and delete post

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,7 +1,46 @@
-class PostController < ApplicationController
-    def index 
-        # post = Post.create({title: "t1" , content: "content1"})
-        post = Post.all
-        puts "+++++++++++++++==#{post.inspect}"
+class PostsController < ApplicationController
+    def index
+     	@posts=Post.all
     end
-   end
+
+    def new 
+      	@post=Post.new
+    end
+
+    def create
+
+        @post = Post.new({title: params[:post][:title], content: params[:post][:content]})
+        if @post.save 
+        redirect_to @post
+        else
+        render :new 
+        end
+
+    end
+
+    def show
+      	@post=Post.find(params[:id])
+        puts "#{@post}"
+    end
+
+    def edit
+      	@post = Post.find(params[:id])
+    end
+
+      def update
+          @post = Post.find(params[:id])
+          if @post.update(post_params)
+              redirect_to @post
+          else
+              render :edit
+          end
+      end
+
+    def destroy
+        @post = post.find(params[:id]) 
+        @post.destroy
+        redirect_to @post
+    end
+
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,14 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get "posts/" ,to: "posts#index"
+  get "posts/new", to: "posts#new" 
+  post "posts/", to: "posts#create"
+  get "posts/:id", to: "posts#show", as: "post"
+  delete "posts/:id", to: "posts#destroy" , as: 'delete_post'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-  get "post" ,to: "post#index"
+end
+
+
 
 end
